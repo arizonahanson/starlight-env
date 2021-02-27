b7dffa8fc1cccd8de1c9a836f29a20dad078243d
Isaac W Hanson <isaac@starlig.ht>
2021-02-27 15:59:22 +0000

    escape \$

--- nixpkgs/zdot/default.nix
+++ nixpkgs/zdot/default.nix
@@ -8,7 +8,7 @@ pkgs.stdenv.mkDerivation {
 
     cat > $out/.zenv <<EOF
     # Only execute this file once per shell.
-    if [ -n "$__ZDOT_ZSHENV_SOURCED" ]; then return; fi
+    if [ -n "\$__ZDOT_ZSHENV_SOURCED" ]; then return; fi
     __ZDOT_ZSHENV_SOURCED=1
     if test -r "\$HOME/.zenv"; then
       source "\$HOME/.zenv"
@@ -17,24 +17,24 @@ pkgs.stdenv.mkDerivation {
 
     cat > $out/.zprofile <<EOF
     # Only execute this file once per shell.
-    if [ -n "$__ZDOT_ZPROFILE_SOURCED" ]; then return; fi
+    if [ -n "\$__ZDOT_ZPROFILE_SOURCED" ]; then return; fi
     __ZDOT_ZPROFILE_SOURCED=1
     if test -r "\$HOME/.zprofile"; then
       source "\$HOME/.zprofile"
     fi
     # Setup login shell init stuff.
-    #eval `dircolors -b /etc/dircolors`
-    if [ -n "$XDG_CACHE_HOME" ]; then
-      mkdir -p "$XDG_CACHE_HOME"
+    #eval \$(dircolors -b /etc/dircolors)
+    if [ -n "\$XDG_CACHE_HOME" ]; then
+      mkdir -p "\$XDG_CACHE_HOME"
     fi
-    if [ -n "$XDG_CONFIG_HOME" ]; then
-      mkdir -p "$XDG_CONFIG_HOME"
+    if [ -n "\$XDG_CONFIG_HOME" ]; then
+      mkdir -p "\$XDG_CONFIG_HOME"
     fi
     EOF
 
     cat > $out/.zshrc <<EOF
     # Only execute this file once per shell.
-    if [ -n "$__ZDOT_ZSHRC_SOURCED" ]; then return; fi
+    if [ -n "\$__ZDOT_ZSHRC_SOURCED" ]; then return; fi
     __ZDOT_ZSHRC_SOURCED=1
     HISTFILE="\$HOME/.zsh_history"
     if test -r "\$HOME/.zshrc"; then
