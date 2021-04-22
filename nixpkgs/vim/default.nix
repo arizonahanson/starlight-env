18371b9f6d8f68c022937314793169677ace1615
Isaac W Hanson <isaac@starlig.ht>
2021-04-22 09:55:39 -0400

    set nocompatible

--- nixpkgs/vim/default.nix
+++ nixpkgs/vim/default.nix
@@ -19,16 +19,12 @@ in
   vimrcConfig =
     {
       beforePlugins = ''
+        set nocompatible
         let g:ale_completion_enabled=1
         let g:ale_completion_delay=1000
         let g:ale_cache_executable_check_failures=1
         let g:ale_close_preview_on_insert=1
-        let g:ale_fixers={
-          \ 'nix': ['nixpkgs-fmt'],
-          \ 'java': ['uncrustify'],
-          \ 'javascript': ['eslint'],
-          \ 'typescript': ['eslint']
-          \ }
+        let g:ale_fixers={ 'nix': ['nixpkgs-fmt'], 'java': ['uncrustify'], 'javascript': ['eslint'], 'typescript': ['eslint'] }
         let g:ale_fix_on_save=1
         let g:ale_hover_to_preview=1
         let g:ale_lint_delay=2000
